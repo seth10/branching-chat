26cad24b95a40dda48642b047bf64f3f15f00f6b
sethten <sethten@users.noreply.github.com>
2016-07-30 22:38:47 -0400

    Begin Socket.IO Tutorial
    Use Express to serve static text over HTTP

---
+++ index.js
@@ -0,0 +1,12 @@
+// following tutorial at http://socket.io/get-started/chat/
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
