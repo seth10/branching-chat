6c4da5685f8cb25c580062d547902902873ccccd
sethten <sethten@users.noreply.github.com>
2016-07-31 18:50:04 -0400

    Add Nickname Support
    Homework 2: Add support for nicknames
    Implement by setting a new property of the socket object. This makes it
    so users can't edit the client code to claim to be an arbitrary
    nickname each message.

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
-  io.emit('user connect');
+  console.log('a user connected (waiting for nickname)');
+
+  socket.on('init nickname', function(nickname) {
+    socket.nickname = nickname;
+    console.log(nickname + ' connected');
+    io.emit('user connect', nickname);
+  });
 
   socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    console.log(socket.nickname + ': ' + msg);
+    io.emit('chat message', socket.nickname + ': ' + msg);
   });
 
   socket.on('disconnect', function() {
-    console.log('user disconnected');
-    io.emit('user disconnect');
+    console.log(socket.nickname + ' disconnected');
+    io.emit('user disconnect', socket.nickname);
   });
 });
 
