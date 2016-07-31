52d7f079453b4e1745253cb7a29cdfd2b1f4e23d
sethten <sethten@users.noreply.github.com>
2016-07-31 00:00:18 -0400

    Cleanup and Style Consistency
    Refactor an append(text) helper method in index.js to handle the jQuery
    of creating a new list item and appending it, making append calls
    easier to read everywhere else.
    Add spaces before opening brace of function arguments. See "Passing
    Anonymous Functions" in
    https://google.github.io/styleguide/javascriptguide.xml#Code_formatting

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('a user connected');
   io.emit('user connect');
-  socket.on('chat message', function(msg){
+
+  socket.on('chat message', function(msg) {
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
-  socket.on('disconnect', function(){
+
+  socket.on('disconnect', function() {
     console.log('user disconnected');
     io.emit('user disconnect');
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
