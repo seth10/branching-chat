40f65fb39f1e5f91e1aa2578e95da73ba2e038b3
sethten <sethten@users.noreply.github.com>
2016-07-30 23:21:46 -0400

    Emit Events, Bidirectional

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
