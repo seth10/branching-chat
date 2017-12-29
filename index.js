053099bcbc783f70c1b0ffdc30b98104d1c61427
Seth T <sethtenembaum@gmail.com>
2017-12-28 19:55:37 -0500

    Minimize and return full chats

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-var AWS = require('aws-sdk');
-
-var dynamodb = new AWS.DynamoDB();
-var docClient = new AWS.DynamoDB.DocumentClient();
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-    docClient.scan(
-        {
-            TableName: "branching.chat",
-            ProjectionExpression: "Id, Participants"
-        },
-        (err, data) => {
-            if (err) console.log(err);
-            let chatIds = data.Items.filter(chat => chat.Participants.values.includes(event.queryStringParameters.name)).map(chat => chat.Id);
-            callback(null, chatIds);
-        }
-    );
+    docClient.scan({TableName:"branching.chat"}, (err, data) => {
+        let chats = data.Items.filter(chat => chat.Participants.values.includes(event.queryStringParameters.name));
+        callback(null, chats);
+    });
 };
