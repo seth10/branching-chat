9de3034b1649406ffb7022236532fed5e8bd5797
Seth T <sethtenembaum@gmail.com>
2017-12-28 21:24:21 -0500

    Clean up chat.js and add test event

--- chat.js
+++ chat.js
@@ -8,15 +8,10 @@ exports.handler = (event, context, callback) => {
             "Id": {N: event.queryStringParameters.id.toString()}
         }
     }, (err, data) => {
-        //console.log(data.Item.Chat.L);
         data.Item.Chat.L.push({S:event.queryStringParameters.name+": "+event.queryStringParameters.message});
-        //console.log(data.Item.Chat.L);
         dynamodb.putItem({
             TableName: "branching.chat",
             Item: data.Item
-        }, (err, data) => {
-            console.log(err);
-            console.log(data);
-        });
+        }, (e,d)=>{});
     });
 };
