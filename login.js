2a97102aad4436687940a9a22776ea6e058e81c7
Seth T <seth10@users.noreply.github.com>
2017-12-30 16:15:43 -0500

    Update login.js to accept a hashed passphrase

--- login.js
+++ login.js
@@ -1,9 +1,40 @@
 const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB();
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
-    docClient.scan({TableName:"branching.chat"}, (err, data) => {
-        let chats = data.Items.filter(chat => chat.Participants.values.includes(event.queryStringParameters.name));
-        callback(null, chats);
+exports.handler = (event, context, realCallback) => {
+    dynamodb.getItem({
+        TableName: "branching.chat_users",
+        Key: {
+            "hash": {S: event.queryStringParameters.hash}
+        }
+    }, (err, data) => {
+        if (data.Item) {
+            callback(data.Item.name.S);
+        } else {
+            if (event.queryStringParameters.name) {
+                dynamodb.putItem({
+                    TableName: "branching.chat_users",
+                    Item: {
+                        hash: {S: event.queryStringParameters.hash},
+                        name: {S: event.queryStringParameters.name},
+                    }
+                }, (err, data) => {
+                    callback("added:"+event.queryStringParameters.name);
+                });
+            } else {
+                callback("unknown");
+            }
+        }
     });
+
+    function callback(body) {
+        realCallback(null, {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+            },
+            'body': body
+        });
+    }
 };
