bf3bddee2e5bb307d3810d995001e838e5676169
Seth T <seth10@users.noreply.github.com>
2017-12-31 21:09:02 -0500

    Use Promises for replacing hashes with names

--- login.js
+++ login.js
@@ -13,39 +13,50 @@ exports.handler = (event, context, realCallback) => {
             let response = {name: data.Item.name.S};
             docClient.scan({TableName:'branching.chat'}, (err, data) => {
                 response.chats = data.Items.filter(chat => chat.Participants.values.includes(event.queryStringParameters.hash));
-                response.chats.forEach(chat => {
-                    let participants = chat.Participants.values;
-                    dynamodb.batchGetItem({
-                        RequestItems: {
-                            'branching.chat_users': {
-                                Keys: participants.map(hash=>({hash: {S: hash}}))
-                            }
-                        }
-                    }, (err, data) => {
-                        participants = data.Responses['branching.chat_users'].reduce((acc,cur)=>{acc[cur.hash.S]=cur.name.S;return acc}, {});
-                        chat.Chat.forEach(message => {
-                            if (message.n) {
-                                message.n = participants[message.n];
-                            }
-                        });
-                    });
-                });
-                setTimeout(()=>{callback(response)}, 1000);
+                Promise.all(response.chats.map(replaceNames)).then(() => callback(response));
             });
         } else if (event.queryStringParameters.name) {
-            dynamodb.putItem({
+			addUsername().then(() => callback({name: event.queryStringParameters.name, 'new': true}));
+		} else {
+			callback({unknown: true});
+		}
+    });
+
+	function replaceNames(chat) {
+		return new Promise(function(resolve, reject) {
+			let participants = chat.Participants.values;
+			dynamodb.batchGetItem({
+				RequestItems: {
+					'branching.chat_users': {
+						Keys: participants.map(hash=>({hash: {S: hash}}))
+					}
+				}
+			}, (err, data) => {
+				participants = data.Responses['branching.chat_users'].reduce((acc,cur)=>{acc[cur.hash.S]=cur.name.S;return acc}, {});
+				chat.Chat.forEach(message => {
+					if (message.n) {
+						message.n = participants[message.n];
+					}
+				});
+				resolve();
+			});
+		});
+	}
+
+	function addUsername(name) {
+        return new Promise(function(resolve, reject) {
+			dynamodb.putItem({
                 TableName: 'branching.chat_users',
                 Item: {
                     hash: {S: event.queryStringParameters.hash},
                     name: {S: event.queryStringParameters.name},
                 }
             }, (err, data) => {
-                callback({name: event.queryStringParameters.name, 'new': true});
+                if (err) reject();
+				else resolve(data);
             });
-        } else {
-            callback({unknown: true});
-        }
-    });
+        });
+    }
 
     function callback(body) {
         realCallback(null, {
